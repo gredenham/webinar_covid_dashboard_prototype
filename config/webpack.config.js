2ca1a8c30256ee85e32f3066370dc6f19683847e
Denis Grekov <denis-grekov@yandex-team.ru>
2021-04-26 16:04:32 +0300

    fix path for static

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -78,7 +78,7 @@ const hasJsxRuntime = (() => {
 
 // This is the production and development configuration.
 // It is focused on developer experience, fast rebuilds, and a minimal bundle.
-module.exports = function (webpackEnv, staticPathPrefix = '') {
+module.exports = function (webpackEnv) {
   const isEnvDevelopment = webpackEnv === 'development';
   const isEnvProduction = webpackEnv === 'production';
 
@@ -201,13 +201,13 @@ module.exports = function (webpackEnv, staticPathPrefix = '') {
       // There will be one main bundle, and one file per asynchronous chunk.
       // In development, it does not produce real files.
       filename: isEnvProduction
-        ? staticPathPrefix + 'static/js/[name].[contenthash:8].js'
+        ? 'static/js/[name].[contenthash:8].js'
         : isEnvDevelopment && 'static/js/bundle.js',
       // TODO: remove this when upgrading to webpack 5
       futureEmitAssets: true,
       // There are also additional JS chunk files if you use code splitting.
       chunkFilename: isEnvProduction
-        ? staticPathPrefix + 'static/js/[name].[contenthash:8].chunk.js'
+        ? 'static/js/[name].[contenthash:8].chunk.js'
         : isEnvDevelopment && 'static/js/[name].chunk.js',
       // webpack uses `publicPath` to determine where the app is being served from.
       // It requires a trailing slash, or the file assets will get an incorrect path.
@@ -638,8 +638,8 @@ module.exports = function (webpackEnv, staticPathPrefix = '') {
         new MiniCssExtractPlugin({
           // Options similar to the same options in webpackOptions.output
           // both options are optional
-          filename: staticPathPrefix + 'static/css/[name].[contenthash:8].css',
-          chunkFilename: staticPathPrefix + 'static/css/[name].[contenthash:8].chunk.css',
+          filename: 'static/css/[name].[contenthash:8].css',
+          chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
         }),
       // Generate an asset manifest file with the following content:
       // - "files" key: Mapping of all asset filenames to their corresponding
